d99118f0191031fbaf0927a4f278676efe4c5e50
SDET185QA@gmail.com <SDET185QA@gmail.com>
2025-01-14 10:34:49 -0500

    allure and extent report support

--- src/test/java/utilities/ExtentReport.java
+++ src/test/java/utilities/ExtentReport.java
@@ -20,15 +20,18 @@ public class ExtentReport {
 		//File extentReporFile=new File(System.getProperty("user.dir")+"target/Extentreports/DsalgoExtend.html");
 		
 		//ExtentSparkReporter sparkReporter=new ExtentSparkReporter(extentReporFile);
-		String timeStamp=new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-		String reportName="Test-Report-"+timeStamp+".html";
-		ExtentSparkReporter sparkReporter=new ExtentSparkReporter("\\ExtentReports\\"+reportName);
+		//String timeStamp=new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+		  //String reportName="Test-Report-"+timeStamp+".html";
+		  String reportName="DSAlgoTestNGExtentReports.html";
+	     //ExtentSparkReporter sparkReporter=new ExtentSparkReporter("\\ExtentReports\\"+reportName);
+	     ExtentSparkReporter sparkReporter=new ExtentSparkReporter(reportName);
 		
 		
-		sparkReporter.config().setTheme(Theme.DARK);
+		sparkReporter.config().setTheme(Theme.STANDARD);
 		sparkReporter.config().setReportName("DSAlgo TestNg Project Result");
 		sparkReporter.config().setDocumentTitle("DSAlgo Automation Report");
 		sparkReporter.config().setTimeStampFormat("MM/dd/yyyy hh:mm:ss");
+		
 	
 		
 		ExtentReports extentReport=new ExtentReports();
@@ -39,6 +42,7 @@ public class ExtentReport {
 		extentReport.setSystemInfo("Operating Systen", System.getProperty("os.name"));
 		extentReport.setSystemInfo("User Name",System.getProperty("user.name"));
 		extentReport.setSystemInfo("Java Version",System.getProperty("java.version") );
+	
 	
 		
 		return extentReport;
